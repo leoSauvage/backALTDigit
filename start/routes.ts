55bc3c3c6e239ff6906a2279398ce89281e22b07
Your Name <you@example.com>
2025-07-16 15:50:29 +0000

    feat(maj)

--- start/routes.ts
+++ start/routes.ts
@@ -28,7 +28,7 @@ Route.get('/api/contact-steps/:stepID', '#controllers/admin_compagnies_controlle
 Route.get('/api/init-contract/:workflowId', '#controllers/admin_compagnies_controller.initContract')
 Route.get('/api/get-step-action/:id', '#controllers/admin_compagnies_controller.initContract')
 
-Route.get('/api/init-contract/:workflowId', '#controllers/admin_compagnies_controller.initContract')
+// Route.get('/api/init-contract/:workflowId', '#controllers/admin_compagnies_controller.initContract')
 Route.post('/api/submit-action', '#controllers/admin_compagnies_controller.submitAction')
 Route.post(
   '/api/get-previous-step-data',
